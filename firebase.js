ccfa255d04f8201b8e1a219e064f63fa1e3c33dd
cesareuseche <cesareuseche@gmail.com>
2021-01-26 21:22:56 -0400

    added the firebase config and intalled the navigation dependencies

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB4TPumN3bIeSYEIEBadYgOWcoo2cgHoZE",
+    authDomain: "signal-26d0f.firebaseapp.com",
+    projectId: "signal-26d0f",
+    storageBucket: "signal-26d0f.appspot.com",
+    messagingSenderId: "383121509911",
+    appId: "1:383121509911:web:997c43ab9dfc5168869652"
+};

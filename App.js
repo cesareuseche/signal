a9aee9242549c0c65f34218731d3936d789ac946
cesareuseche <cesareuseche@gmail.com>
2021-01-29 14:47:45 -0400

    started working on the LoginScreen Component

--- App.js
+++ App.js
@@ -7,11 +7,16 @@ import LoginScreen from './screenscomponents/LoginScreen';
 import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
+const globalScreenOptions = {
+  headerStyle: { backgroundColor: "#2C6BED" },
+  headerTitleStyle: { color: "#FFFFFF" },
+  headerTintColor: "#FFFFFFF",
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={globalScreenOptions} >
         <Stack.Screen name='Login' component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- screenscomponents/LoginScreen.js
+++ screenscomponents/LoginScreen.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-
+import { Botton, Input, Image } from "react-native-elements"
+import { StatusBar } from 'expo-status-bar'
 const LoginScreen = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("");
     return (
         <View>
-            <Text>this is the login</Text>
+            <StatusBar style="light" />
+            <Image source={{
+                uri:
+                    "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png",
+            }}
+                style={{ width: 200, height: 200 }} />
+            <View style={StyleSheet.InputContainer}>
+                <Input placeholder="Email" autoFocus type="Email" value={email} onChangeText={(text) => setEmail(text)} />
+                <Input placeholder="Password" secureTextEntry type="password" value={password} />
+            </View>
         </View>
     )
 }
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    InputContainer: {
+
+    },
+})

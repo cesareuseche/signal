ce15ff4fe1c9794320c348b7a58b0c8b9b49fc9f
cesareuseche <cesareuseche@gmail.com>
2021-05-11 19:22:50 -0400

    current user image done home screen

--- screenscomponents/HomeScreen.js
+++ screenscomponents/HomeScreen.js
@@ -1,17 +1,30 @@
 import React, { useLayoutEffect } from 'react'
 import { StyleSheet, ScrollView, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native'
+import { Avatar } from 'react-native-elements'
+import { auth } from '../firebase';
+
+
 //importing components
 import CustomListItem from '../components/CustomListItem'
-
 
 const HomeScreen = ({ navigation }) => {
 
     useLayoutEffect(() => {
         navigation.setOptions({
             title: "Signal",
-        })
-    }, [])
+            headerStyle: { backgroundColor: "#fff" },
+            headerTitleStyle: { color: "#000" },
+            headerTintColor: "black",
+            headerLeft: () => (
+                <View style={{ marginLeft: 20 }}>
+                    <Avatar rounded source={{ uri: auth?.currentUser?.photoURL }}>
+
+                    </Avatar>
+                </View>
+            )
+        });
+    }, []);
 
     return (
         <SafeAreaView>

859cad0abd0eb94e27ecf532d575baa7d7932ff3
cesareuseche <cesareuseche@gmail.com>
2021-05-12 19:40:56 -0400

    create chat functionality

--- screenscomponents/AddChatScreen.js
+++ screenscomponents/AddChatScreen.js
@@ -2,9 +2,24 @@ import React, { useLayoutEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Button, Input } from "react-native-elements"
 import Icon from "react-native-vector-icons/Ionicons"
+// Importing the db from firebase
+import { db } from '../firebase';
 
 const AddChatScreen = ({ navigation }) => {
     const [input, setInput] = useState("");
+
+    //Create chat async function
+    const createChat = async () => {
+        await db
+            .collection('chats')
+            .add({
+                chatName: input,
+            })
+            .then(() => {
+                navigation.goBack()
+            })
+            .catch((error) => alert(error));
+    }
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -19,10 +34,13 @@ const AddChatScreen = ({ navigation }) => {
                 placeholder="Enter a chat name"
                 value={input}
                 onChangeText={(text) => setInput(text)}
+                onSubmitEditing={createChat}
                 leftIcon={
                     <Icon name="chatbubbles-outline" type="antdesign" size={24} color="#000" />
                 }
             />
+            <Button onPress={createChat} title={'New Chat'} />
+
         </View>
     )
 }

4944bf41e485e476fac7dcddd9a8d210e289c45a
cesareuseche <cesareuseche@gmail.com>
2021-05-13 22:09:26 -0400

    useLayoutEffect on the chatscreen

--- screenscomponents/ChatScreen.js
+++ screenscomponents/ChatScreen.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+//Avatar placeholder
+import { Avatar } from 'react-native-elements';
 
 const ChatScreen = ({ navigation, route }) => {
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: "Chat",
+            headerBackTitleVisible: false,
+            headerTitleAlign: "left",
+            headerTitle: () => (
+                <View
+                    style={{
+                        flexDirection: "row",
+                        alignItems: "center",
+                    }}
+                >
+                    <Avatar rounded source={{
+                        uri:
+                            "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
+                    }} />
+                    <Text>{route.params.chatName}</Text>
+                </View>
+            )
+        })
+    }, [navigation])
+
     return (
         <View>
             <Text>{route.params.chatName}</Text>

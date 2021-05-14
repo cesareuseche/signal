657ce86ed3181e5c2663f88f02ad75c820589545
cesareuseche <cesareuseche@gmail.com>
2021-05-13 21:49:43 -0400

    adding route to the chat

--- screenscomponents/ChatScreen.js
+++ screenscomponents/ChatScreen.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-const ChatScreen = () => {
+const ChatScreen = ({ navigation, route }) => {
     return (
         <View>
-            <Text>Chat Screen</Text>
+            <Text>{route.params.chatName}</Text>
         </View>
     )
 }

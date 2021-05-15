61db88e9d8a94af2c32e5bbea868ee535c941979
cesareuseche <cesareuseche@gmail.com>
2021-05-15 17:58:15 -0400

    chat photo component

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { ListItem, Avatar } from 'react-native-elements'
+import { db } from '../firebase';
 
 const CustomListItem = ({ id, chatName, enterChat }) => {
+    const [chatMessages, setChatMessages] = useState([]);
+
+    useEffect(() => {
+        const unsubscribe = db
+            .collection('chats')
+            .doc(id)
+            .collection("messages")
+            .orderBy('timestamp', 'desc')
+            .onSnapshot((snapshot) => {
+                setChatMessages(snapshot.docs.map((doc) => doc.data()))
+            })
+
+        return unsubscribe;
+    })
+
     return (
         <ListItem
+            key={id}
             onPress={() => enterChat(id, chatName)}
             key={id}
             bottomDividder>
             <Avatar
                 rounded
                 source={{
-                    uri:
+                    uri: chatMessages?.[0]?.photoURL ||
                         "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png",
 
                 }}
